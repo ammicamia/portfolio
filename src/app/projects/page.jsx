efeb9f42e8d2752ca09c13d98756d7d8e1d4e2b0
ammicamia <camiammiruth@gmail.com>
2026-07-16 21:20:19 +1200

    "deleted a project as it hasn't been deployed yet"

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -10,7 +10,6 @@ export default function Projects() {
                 <h1>Projects</h1>
                 <div className={styles.receipts}>
                     <Receipt number="001" title="PURRBIT" description="A tamagotchi-style game for Chrome extensions, but cats. This was created for a cat-themed hackathon." stack={["HTML", "CSS", "JavaScript"]} github="https://github.com/kesauce/purrbit" />
-                    <Receipt number="002" title="VESPERIA DESIGN STUDIOS" description="A website for an interior designer built with a CMS so she can update her portfolio and other images without touching code." stack={["Next.js", "Sanity", "Resend", "Framer Motion"]} site="" github="https://github.com/kesauce/purrbit" />
                 </div>
             </div>
         </div>
